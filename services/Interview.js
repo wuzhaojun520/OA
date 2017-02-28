221b80db5f77388622c1c45ac8689e1034da9f1c
TomLeel <lt1994outlook@163.com>
2017-02-28 19:30:01 +0800

    TomLee

---
+++ services/Interview.js
@@ -0,0 +1,55 @@
+/**
+ * Created by LeeTao on 2017/2/28.
+ */
+(function () {
+
+    //面试官
+    var HR = {
+        name:"王女士"
+    };
+
+    //面试者
+    var Interviewer = [
+        {
+            name:"张三",
+            phoneNumber:"110",
+            time:"2017-3-1"
+        },
+        {
+            name:"李四",
+            phoneNumber:"120",
+            time:"2017-3-1"
+        },
+        {
+            name:"王麻子",
+            phoneNumber:"114",
+            time:"2017-3-1"
+        }
+    ];
+
+    //面试题
+    var topic = [
+        {
+            question:"1、什么是Java虚拟机？为什么Java被称作是“平台无关的编程语言”？"
+        },{
+            question:"2、JDK和JRE的区别是什么？"
+        },{
+            question:"3、”static”关键字是什么意思？Java中是否可以覆盖(override)一个private或者是static的方法？"
+        },{
+            question:"4、是否可以在static环境中访问非static变量？"
+        },{
+            question:"5、Java支持的数据类型有哪些？什么是自动拆装箱？"
+        },{
+            question:"6、Java中的方法覆盖(Overriding)和方法重载(Overloading)是什么意思？"
+        },{
+            question:"7、Java支持多继承么？"
+        },{
+            question:"8、接口和抽象类的区别是什么？"
+        },{
+            question:"9、什么是值传递和引用传递？"
+        },{
+            question:"10、进程和线程的区别是什么？"
+        }
+    ];
+
+})();
