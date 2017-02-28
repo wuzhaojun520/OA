b0ba4268233b7a93369fa8ebcbe415111152d23f
Huang <838184712@qq.com>
2017-02-28 17:16:36 +0800

    server

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Huang on 2017/2/28.
+ */
+(function (angular) {
+    angular.module("Home", [])
+        .controller("HomeController", ["$scope", function ($scope) {
+
+        }]);
+
+
+})(window.angular);
